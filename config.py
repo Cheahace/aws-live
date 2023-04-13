bd6dbfe0ef45d9a7bdc67d2c9eceb5a57faf403e
Cheahace <87080476+Cheahace@users.noreply.github.com>
2023-04-13 15:11:51 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cdvvcgampsrc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheahchinhong-employee"
+customregion = "us-east-1"
